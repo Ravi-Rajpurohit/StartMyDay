3be9398ad9e7abc3b3738bab9c6a83efa675a8aa
ravindra <ravi.rajpurohit385@gmail.com>
2018-02-27 19:38:52 +0530

    added instructions

--- StartMyDay.sh
+++ StartMyDay.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
 (code -n -g ~/Documents/basixs-SuperAdmin/basixsWebApp-SuperAdmin &)
+# sleep
 (code -n -g ~/Documents/basixs-SuperAdmin/basixsService-SuperAdmin &)
 (google-chrome --new-window http://192.168.1.10:8090/ &)
 echo 'heyy'
 exit 0
+
+# You could simply add the following command into System > Preferences > Startup Applications:
+
+# bash /full/path/to/your/script.sh
+# That should do the trick ;)
